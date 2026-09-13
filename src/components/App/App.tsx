b739550c55fd5e9728ea6e960409383ff7fb5136
Maxim <ratutuy55@gmail.com>
2026-09-14 00:04:44 +0300

    update components

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,13 +1,13 @@
 import { useState } from 'react';
 import styles from './App.module.css';
-import SearchBar from './components/SearchBar/SearchBar';
-import fetchMovies from './services/movieService';
-import type { Movie } from './types/movie';
+import SearchBar from '../SearchBar/SearchBar';
+import fetchMovies from '../../services/movieService';
+import type { Movie } from '../../types/movie';
 import toast, { Toaster } from 'react-hot-toast';
-import ErrorMessage from './components/ErrorMessage/ErrorMessage';
-import Loader from './components/Loader/Loader';
-import MovieGrid from './components/MovieGrid/MovieGrid';
-import MovieModal from './components/MovieModal/MovieModal';
+import ErrorMessage from '../ErrorMessage/ErrorMessage';
+import Loader from '../Loader/Loader';
+import MovieGrid from '../MovieGrid/MovieGrid';
+import MovieModal from '../MovieModal/MovieModal';
 function App() {
   const [movies, setMovies] = useState<Movie[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
